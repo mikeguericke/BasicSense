0e8a722e5c6222697bb931404089bb43586842fc
mikeguericke <mike.guericke@gmail.com>
2017-02-08 13:57:23 -0600

    Added images and image software.  Use rgbpaint for photo creation

---
+++ imageSense.py
@@ -0,0 +1,62 @@
+#Set up the sense hat module in python
+from sense_hat import SenseHat
+import os
+import time
+sense = SenseHat()
+
+#Set up Variables
+delay = 0.5
+
+#Set up the color pallet
+R = [255, 0, 0]
+G = [0, 255, 0]
+B = [0, 0, 255]
+X = [0, 0, 0]
+W = [255, 255, 255]
+#O = [255, 165, 0]
+
+#Set up the three scenes
+dispOne = [
+    W, W, W, R, R, W, W, W,
+    W, W, R, R, R, R, W, W,
+    W, R, R, R, R, R, R, W,
+    R, R, R, R, R, R, R, R,
+    W, W, W, R, R, W, W, W,
+    W, W, W, R, R, W, W, W,
+    W, W, W, R, R, W, W, W,
+    W, W, W, R, R, W, W, W,
+    ]
+
+dispTwo = [
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    ]
+
+dispThree = [
+    B, B, B, B, B, B, B, B,
+    B, B, B, B, B, B, B, B,
+    G, G, G, G, G, G, G, G,
+    G, G, G, G, G, G, G, G,
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    X, X, X, X, X, X, X, X,
+    X, X, X, X, X, X, X, X,
+    ]
+
+#Excute code
+while True:
+    sense.clear()
+    sense.load_image("/home/pi/BasicSense/images/test.png")
+    time.sleep(delay)
+    sense.clear()
+    sense.load_image("/home/pi/BasicSense/images/test2.png")
+    time.sleep(delay)
+    sense.clear()
+    sense.load_image("/home/pi/BasicSense/images/test3.png")
+    time.sleep(delay)
