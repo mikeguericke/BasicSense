424e7ab3f3c526c67e191cbdb0b83dc1332e22c1
Mike G <mike.guericke@gmail.com>
2017-02-08 11:49:18 -0600

    Original File

---
+++ BasicSense.py
@@ -0,0 +1,53 @@
+#Set up the sense hat module in python
+from sense_hat import senseHat
+import os
+import time
+sense = SenseHat()
+
+#Set up the color pallet
+R = [255, 0, 0]
+G = [0, 255, 0]
+B = [0, 0, 255]
+
+#Set up the three scenes
+dispOne = [
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    ]
+
+dispTwo = [
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    ]
+
+dispThree = [
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    ]
+
+#Excute code
+while True:
+    sense.set_pixels(dispOne)
+    time.sleep(0.1)
+    sense.set_pixels(dispTwo)
+    time.sleep(0.1)
+    sense.set_pixels(dispThree)
+    time.sleep(0.1)
