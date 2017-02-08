934dddb50e58b9b437c436b0f14186b8ae22bb7d
mikeguericke <mike.guericke@gmail.com>
2017-02-08 12:04:39 -0600

    Increased functionality and added colors

--- BasicSense.py
+++ BasicSense.py
@@ -1,24 +1,30 @@
 #Set up the sense hat module in python
-from sense_hat import senseHat
+from sense_hat import SenseHat
 import os
 import time
 sense = SenseHat()
+
+#Set up Variables
+delay = 0.1
 
 #Set up the color pallet
 R = [255, 0, 0]
 G = [0, 255, 0]
 B = [0, 0, 255]
+X = [255, 255, 255]
+W = [0, 0, 0]
+#O = [255, 165, 0]
 
 #Set up the three scenes
 dispOne = [
+    W, W, W, R, R, W, W, W,
+    W, W, R, R, R, R, W, W,
+    W, R, R, R, R, R, R, W,
     R, R, R, R, R, R, R, R,
-    R, R, R, R, R, R, R, R,
-    R, R, R, R, R, R, R, R,
-    R, R, R, R, R, R, R, R,
-    R, R, R, R, R, R, R, R,
-    R, R, R, R, R, R, R, R,
-    R, R, R, R, R, R, R, R,
-    R, R, R, R, R, R, R, R,
+    W, W, W, R, R, W, W, W,
+    W, W, W, R, R, W, W, W,
+    W, W, W, R, R, W, W, W,
+    W, W, W, R, R, W, W, W,
     ]
 
 dispTwo = [
@@ -45,9 +51,12 @@ dispThree = [
 
 #Excute code
 while True:
+    sense.clear()
     sense.set_pixels(dispOne)
-    time.sleep(0.1)
+    time.sleep(delay)
+    sense.clear()
     sense.set_pixels(dispTwo)
-    time.sleep(0.1)
+    time.sleep(delay)
+    sense.clear()
     sense.set_pixels(dispThree)
-    time.sleep(0.1)
+    time.sleep(delay)
