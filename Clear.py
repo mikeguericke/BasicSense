27b448bf5537a4df39e48bfffc75b39a7cbb8b01
mikeguericke <mike.guericke@gmail.com>
2017-02-09 12:36:38 -0600

    Changed Urban League to Step Up

--- Clear.py
+++ Clear.py
@@ -2,5 +2,5 @@ from sense_hat import SenseHat
 sense = SenseHat()
 
 sense.clear()
-sense.show_message("Thank You UrbanLeague", text_colour=[0,0,255])
+sense.show_message("Thank You Step Up", text_colour=[0,0,255])
 sense.clear()
