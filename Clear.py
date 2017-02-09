2527edd20c241c8db1abcbcbd8efc213b7fe2898
mikeguericke <mike.guericke@gmail.com>
2017-02-09 12:22:33 -0600

    Added personal thanks

--- Clear.py
+++ Clear.py
@@ -2,4 +2,5 @@ from sense_hat import SenseHat
 sense = SenseHat()
 
 sense.clear()
-sense.show_message("You are Clear")
+sense.show_message("Thank You UrbanLeague", text_colour=[0,0,255])
+sense.clear()
